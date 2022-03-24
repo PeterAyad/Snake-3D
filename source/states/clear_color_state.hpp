e50f02ca2ca6834ef34084ce33692ccda4013554
peterayad <peter.ayad00@eng-st.cu.edu.eg>
2022-03-24 18:50:27 +0200

    clearColor Done

--- source/states/clear_color_state.hpp
+++ source/states/clear_color_state.hpp
@@ -10,6 +10,15 @@ class ClearColorState: public our::State {
         //To see how the clear color is written in the json files, see "config/blue-screen.json"
         //To know how read data from a nlohmann::json object, 
         //look at the following documentation: https://json.nlohmann.me/features/element_access/
+
+        nlohmann::json clearColor = getApp()->getConfig().at("scene").at("clear-color");
+        float r = clearColor["r"].get<float>();
+        float g = clearColor["g"].get<float>();
+        float b = clearColor["b"].get<float>();
+        float a = clearColor["a"].get<float>();
+        
+        // set color you want to clear the buffers with
+        glClearColor(r,g,b,a);
     }
 
     // onDraw(deltaTime) function is called every frame 
