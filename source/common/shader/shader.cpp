b23eee3b3f756ef1cc38a20b2be0cdbe6f02ca7e
peterayad <peter.ayad00@eng-st.cu.edu.eg>
2022-03-24 19:55:40 +0200

    Shader Class Done

--- source/common/shader/shader.cpp
+++ source/common/shader/shader.cpp
@@ -34,18 +34,25 @@ bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const 
     GLuint shaderID = glCreateShader(type);
 
     //TODO: send the source code to the shader and compile it
+    // Link the shader with its source string
+    glShaderSource(shaderID, 1, &sourceCStr, nullptr);
+
+    // Compile the shader source
+    glCompileShader(shaderID);
     
     // Here we check for compilation errors
     //TODO: Uncomment this if block
-    // if(std::string error = checkForShaderCompilationErrors(shaderID); error.size() != 0){
-    //     std::cerr << "ERROR IN " << filename << std::endl;
-    //     std::cerr << error << std::endl;
-    //     glDeleteShader(shaderID);
-    //     return false;
-    // }
+    if(std::string error = checkForShaderCompilationErrors(shaderID); error.size() != 0){
+        std::cerr << "ERROR IN " << filename << std::endl;
+        std::cerr << error << std::endl;
+        glDeleteShader(shaderID);
+        return false;
+    }
 
     
     //TODO: attach the shader to the program then delete the shader
+    glAttachShader(this->program, shaderID);
+    glDeleteShader(shaderID);
 
     //We return true since the compilation succeeded
     return true;
@@ -55,14 +62,15 @@ bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const 
 
 bool our::ShaderProgram::link() const {
     //TODO: call opengl to link the program identified by this->program 
+    glLinkProgram(this->program);
 
     // Here we check for linking errors
     //TODO: Uncomment this if block
-    // if(auto error = checkForLinkingErrors(program); error.size() != 0){
-    //     std::cerr << "LINKING ERROR" << std::endl;
-    //     std::cerr << error << std::endl;
-    //     return false;
-    // }
+    if(auto error = checkForLinkingErrors(program); error.size() != 0){
+        std::cerr << "LINKING ERROR" << std::endl;
+        std::cerr << error << std::endl;
+        return false;
+    }
 
     return true;
 }
--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -28,30 +28,37 @@ namespace our {
 
         void use() { 
             //TODO: call opengl to use the program identified by this->program
+            glUseProgram(this->program);
         }
 
         GLuint getUniformLocation(const std::string &name) {
             //TODO: call opengl to get the uniform location for the uniform defined by name from this->program
+            return glGetUniformLocation(this->program, name.c_str());
         }
 
         void set(const std::string &uniform, GLfloat value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform1f(getUniformLocation(uniform), value);
         }
 
         void set(const std::string &uniform, glm::vec2 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform2fv(getUniformLocation(uniform),1,&value[0]);
         }
 
         void set(const std::string &uniform, glm::vec3 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform3fv(getUniformLocation(uniform),1,&value[0]);
         }
 
         void set(const std::string &uniform, glm::vec4 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform4fv(getUniformLocation(uniform),1,&value[0]);
         }
 
-
         //TODO: Delete the copy constructor and assignment operator
+        ShaderProgram(const ShaderProgram &x) = delete;
+        void operator=(ShaderProgram x) = delete;
         //Question: Why do we do this? Hint: Look at the deconstructor
     };
 
